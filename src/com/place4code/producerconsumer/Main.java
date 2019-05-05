90802c3cc3577e1b39b1ab6d88b5f80d6b754ab7
place4code <place4code@gmail.com>
2019-05-05 17:48:29 +0200

    ExecutorService

--- src/com/place4code/producerconsumer/Main.java
+++ src/com/place4code/producerconsumer/Main.java
@@ -4,6 +4,8 @@ import com.place4code.mythreads.ThreadsColors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
@@ -11,11 +13,27 @@ public class Main {
 
         List<String> buffer = new ArrayList<>();
         ReentrantLock lock = new ReentrantLock();
+
+        // my thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
         Producer  producer = new Producer(buffer, ThreadsColors.ANSI_BLUE, lock);
         Consumer consumer = new Consumer(buffer, ThreadsColors.ANSI_RED, lock);
+        Consumer consumer2 = new Consumer(buffer, ThreadsColors.ANSI_PURPLE, lock);
+        Consumer consumer3 = new Consumer(buffer, ThreadsColors.ANSI_CYAN, lock);
 
-        new Thread(producer).start();
-        new Thread(consumer).start();
+        // execute threads
+        executorService.execute(producer);
+        executorService.execute(consumer);
+        executorService.execute(consumer2);
+        executorService.execute(consumer3);
+        // shutdown threads pool !!! important
+        executorService.shutdown();
+
+//        new Thread(producer).start();
+//        new Thread(consumer).start();
+//        new Thread(consumer2).start();
+//        new Thread(consumer3).start();
 
     }
 }
